713f9a2705907f1a0149038b65cc23d4fbf38869
Rishabh Gupta <belikerishabh@gmail.com>
2020-02-06 02:23:38 +0530

    deploying

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const monk = require('monk');
-const connectionURL = process.env.MONGODB_URI || 'localhost/shortify';
+const connectionURL = "mongodb+srv://imrishabh18:shortyfycluster0-yk3h6.mongodb.net/test?retryWrites=true&w=majority";
 const db = monk(connectionURL);
 
 module.exports = db;
