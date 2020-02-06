7e02f718495dced3a115717455a01e86fe62f7ec
Rishabh Gupta <belikerishabh@gmail.com>
2020-02-06 21:52:09 +0530

    FinnallY

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 
 var mongo = require('mongodb');
 var monk = require('monk');
-const connectionString = process.env.MONGODB_URI || 'localhost/shortyfy';
+const connectionString = 'mongodb+srv://imrishabh18:1234@shortify-yk3h6.mongodb.net/test?retryWrites=true&w=majority';
 var db = monk(connectionString);
 db.then(() =>{
   console.log("connection success");
